a14ee0e65c3478f0f2fe8d58703a98a86e586deb
hadar-g <hadar.goldstein@gmail.com>
2022-01-21 23:42:39 +0200

    first comitt from vscode test

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>He llo world</Text>
+      <Text>testing here</Text>
       <StatusBar style="auto" />
     </View>
   );
