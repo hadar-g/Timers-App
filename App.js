073945d1ddf6b8728a0f1972e73218ba71bf9a2f
hadar-g <hadar.goldstein@gmail.com>
2022-01-21 23:41:13 +0200

    comit test

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
+      <Text>He llo world</Text>
       <StatusBar style="auto" />
     </View>
   );
